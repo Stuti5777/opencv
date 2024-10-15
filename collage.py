2c49632b663638aecd38abff0ec5249c8f8c57e1
Stuti5777 <69379355+Stuti5777@users.noreply.github.com>
2024-10-15 17:35:57 +0530

    Update collage.py

--- collage.py
+++ collage.py
@@ -8,82 +8,57 @@ print(os.getcwd())
 
 # Folder which contains all the images 
 # from which video is to be generated 
-os.chdir(r"C:\Users\Lenovo\Downloads\collage\newfolder2") 
-path = r"C:\Users\Lenovo\Downloads\collage\newfolder2"
+image_folder = r"/Users/YourUsername/Downloads/collage/newfolder2"  # Update to your macOS path
+
+# Change working directory to the image folder
+os.chdir(image_folder)
 
 mean_height = 0
 mean_width = 0
 
-num_of_images = len(os.listdir('.')) 
-# print(num_of_images) 
+# Count number of images in the folder
+num_of_images = len([file for file in os.listdir(image_folder) if file.endswith(('.jpg', '.jpeg', '.png'))])
 
-for file in os.listdir('.'): 
-	im = Image.open(os.path.join(path, file)) 
-	width, height = im.size 
-	mean_width += width 
-	mean_height += height 
-	# im.show() # uncomment this for displaying the image 
+for file in os.listdir(image_folder): 
+    if file.endswith(('.jpg', '.jpeg', '.png')):
+        im = Image.open(os.path.join(image_folder, file)) 
+        width, height = im.size 
+        mean_width += width 
+        mean_height += height 
 
 # Finding the mean height and width of all images. 
-# This is required because the video frame needs 
-# to be set with same width and height. Otherwise 
-# images not equal to that width height will not get 
-# embedded into the video 
 mean_width = int(mean_width / num_of_images) 
 mean_height = int(mean_height / num_of_images) 
 
-# print(mean_height) 
-# print(mean_width) 
-
-# Resizing of the images to give 
-# them same width and height 
-for file in os.listdir('.'): 
-	if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith("png"): 
-		# opening image using PIL Image 
-		im = Image.open(os.path.join(path, file)) 
-
-		# im.size includes the height and width of image 
-		width, height = im.size 
-		print(width, height) 
-
-		# resizing 
-		imResize = im.resize((mean_width, mean_height), Image.LANCZOS) 
-		imResize.save( file, 'JPEG', quality = 95) # setting quality 
-		# printing each resized image name 
-		print(im.filename.split('\\')[-1], " is resized") 
-
+# Resizing all images to the same size
+for file in os.listdir(image_folder): 
+    if file.endswith(('.jpg', '.jpeg', '.png')): 
+        im = Image.open(os.path.join(image_folder, file)) 
+        imResize = im.resize((mean_width, mean_height), Image.LANCZOS) 
+        imResize.save(file, 'JPEG', quality=95) 
 
 # Video Generating function 
 def generate_video(): 
-	image_folder = '.' # make sure to use your folder 
-	video_name = 'mygeneratedvideo.avi'
-	os.chdir(r"C:\Users\Lenovo\Downloads\collage\newfolder2") 
-	
-	images = [img for img in os.listdir(image_folder) 
-			if img.endswith(".jpg") or
-				img.endswith(".jpeg") or
-				img.endswith("png")] 
-	
-	# Array images should only consider 
-	# the image files ignoring others if any 
-	print(images) 
+    video_name = 'mygeneratedvideo.avi'
+    
+    # Get all image files
+    images = [img for img in os.listdir(image_folder) if img.endswith(('.jpg', '.jpeg', '.png'))]
+    print(images) 
 
-	frame = cv2.imread(os.path.join(image_folder, images[0])) 
+    # Read the first image to set dimensions
+    frame = cv2.imread(os.path.join(image_folder, images[0])) 
+    height, width, layers = frame.shape 
 
-	# setting the frame width, height width 
-	# the width, height of first image 
-	height, width, layers = frame.shape 
+    # Define the video codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')  # Use a codec compatible with Mac
+    video = cv2.VideoWriter(video_name, fourcc, 1, (width, height)) 
 
-	video = cv2.VideoWriter(video_name, 0, 1, (width, height)) 
-
-	# Appending the images to the video one by one 
-	for image in images: 
-		video.write(cv2.imread(os.path.join(image_folder, image))) 
-	
-	# Deallocating memories taken for window creation 
-	cv2.destroyAllWindows() 
-	video.release() # releasing the video generated 
-
+    # Appending the images to the video one by one 
+    for image in images: 
+        video.write(cv2.imread(os.path.join(image_folder, image))) 
+    
+    # Release the video
+    video.release() 
 
 # Calling the generate_video function 
 generate_video() 
